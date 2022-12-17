511f7291927eaca718561007790313d4c5ed1f17
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-12-17 09:40:30 +0000

    a LOT of changes

---
+++ models/Alcohol.model.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const alcoholSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    name: String,
+    description: String,
+    rating: Number,
+    percentage: Number,
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model("Alcohol", alcoholSchema);
---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("alcohol-rater-2 JS imported successfully!");
+});
